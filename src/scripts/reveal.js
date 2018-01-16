a21f9fe41feb13c60ba3dacdd7e652a783d22cfc
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-01-16 20:33:26 +0100

    Add block reveal effect

--- src/scripts/reveal.js
+++ src/scripts/reveal.js
@@ -2,16 +2,35 @@
 import inView from 'in-view';
 
 const ACTIVE_CLASS = 'active';
+const REVEAL_PROFILE_SELECTOR = '.js-profile';
+const REVEAL_BLOCK_SELECTOR = '.js-reveal-block';
 
 class Reveal {
   init() {
     this.revealProfiles();
+    this.revealBlocks();
   }
 
   revealProfiles() {
     inView.offset(200);
-    inView('.js-profile')
+    inView(REVEAL_PROFILE_SELECTOR)
       .on('enter', el => el.classList.add(ACTIVE_CLASS));
+  }
+
+  revealBlocks() {
+    inView.offset(0);
+    inView(REVEAL_BLOCK_SELECTOR)
+      .on('enter', (el) => {
+        const randNum = Math.floor(Math.random() * 30) + 1;
+        const timeout = 20 * randNum;
+        setTimeout(() => el.classList.add(ACTIVE_CLASS), timeout);
+      });
+
+    const blocks = document.querySelectorAll(REVEAL_BLOCK_SELECTOR);
+
+    blocks.forEach((block) => {
+      block.addEventListener('transitionend', () => block.classList.add('js-reveal-text'));
+    });
   }
 }
 
