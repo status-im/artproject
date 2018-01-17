9e7d457bfea189abb07fe839dcb498ef0d7d10f6
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-01-18 00:03:28 +0100

    Enable parallax only from lg up

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -3,11 +3,22 @@ import Rellax from 'rellax';
 const PARALLAX_CLASS = '.js-parallax';
 
 export default function init() {
-  return new Rellax(PARALLAX_CLASS, {
-    speed: -2,
-    center: false,
-    round: true,
-    vertical: true,
-    horizontal: false
+  let rellax;
+
+  window.addEventListener('resize', () => {
+    const width = window.innerWidth;
+
+    if (width >= 1200 && !rellax) {
+      rellax = new Rellax(PARALLAX_CLASS, {
+        speed: -2,
+        center: false,
+        round: true,
+        vertical: true,
+        horizontal: false
+      });
+    } else if (rellax && rellax.destroy && width < 1140) {
+      rellax.destroy();
+      rellax = undefined;
+    }
   });
 }
