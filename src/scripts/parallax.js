9db307cd9b4e81171be57f79eba15aa665bc45df
Maciej Matuszewski <maciek.matuszewski@gmail.com>
2018-01-18 00:17:38 +0100

    Fix parallax initialization

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -2,21 +2,26 @@ import Rellax from 'rellax';
 
 const PARALLAX_CLASS = '.js-parallax';
 
+function initializeRellax() {
+  return new Rellax(PARALLAX_CLASS, {
+    speed: -2,
+    center: false,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
+}
+
 export default function init() {
   let rellax;
+  initializeRellax();
 
   window.addEventListener('resize', () => {
     const width = window.innerWidth;
 
-    if (width >= 1200 && !rellax) {
-      rellax = new Rellax(PARALLAX_CLASS, {
-        speed: -2,
-        center: false,
-        round: true,
-        vertical: true,
-        horizontal: false
-      });
-    } else if (rellax && rellax.destroy && width < 1140) {
+    if (width >= 1200 && rellax === undefined) {
+      rellax = initializeRellax();
+    } else if (rellax && rellax.destroy && width < 1200) {
       rellax.destroy();
       rellax = undefined;
     }
